4d85608264919f6eb8739236b4b5c922f93cae8e
Hiroki-Nemoto <91859484+Hiroki-Nemoto@users.noreply.github.com>
2021-11-04 16:55:52 +0900

    Update test_ArrayStack.cpp

--- ex05/Question1/test_ArrayStack.cpp
+++ ex05/Question1/test_ArrayStack.cpp
@@ -26,32 +26,5 @@ int main(void) {
     stack2.push(9.0);
     stack2.push(10.0);
 
-    // Copy
-    ArrayStack stack3(stack1);
-    ArrayStack stack4 = stack2;
-
-    // Assignment
-    ArrayStack stack5;
-    stack5 = stack1;
-    ArrayStack stack6(10);
-    stack6 = stack6;
-
-    // COMPLETE 
-    // Show the content of the stacks 'stack1', 'stack3' and 'stack4' by calls to pop() and top() 
-    // (use a loop and the method ArrayStack::empty() for the termination condition)
-    while(!stack1.empty()){
-        std::cout <<  stack1.top() << std::endl;
-        stack1.pop();
-    }
-
-    while(!stack3.empty()){
-        std::cout <<  stack3.top() << std::endl;
-        stack3.pop();
-    }
-    while(!stack4.empty()){
-        std::cout <<  stack4.top() << std::endl;
-        stack4.pop();
-    }
-
     return 0;
 }
