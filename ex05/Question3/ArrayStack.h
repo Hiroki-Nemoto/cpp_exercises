b3e9c9fdf3b1e52bf0ba92040f772e35a5712247
Hiroki-Nemoto <91859484+Hiroki-Nemoto@users.noreply.github.com>
2021-11-18 12:11:16 +0900

    Update ArrayStack.h

--- ex05/Question3/ArrayStack.h
+++ ex05/Question3/ArrayStack.h
@@ -36,7 +36,7 @@ public:
     ArrayStack(const ArrayStack& a) {
         _num_items = a._num_items;
         _allocated_size = a._allocated_size;
-        _items = new double[a._num_items];
+        _items = new double[a._allocated_size];
         _items = a._items;
         std::cout << "Copy Constructor" << std::endl;
     }
@@ -44,7 +44,7 @@ public:
     ArrayStack& operator=(const ArrayStack& b){
         _num_items = b._num_items;
         _allocated_size = b._allocated_size;
-        _items = new double[b._num_items];
+        _items = new double[b._allocated_size];
         _items = b._items;
         std::cout << "Assignment Operator" << std::endl;
         return *this;
