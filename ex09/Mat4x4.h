ea23f5fdf2fe4147509e22e698ede6809eef2221
Hiroki-Nemoto <91859484+Hiroki-Nemoto@users.noreply.github.com>
2021-11-22 17:03:04 +0900

    Add files via upload

---
+++ ex09/Mat4x4.h
@@ -0,0 +1,54 @@
+//Mat4x4.h
+#include <cstdlib>
+#include <istream>
+#include <ostream>
+#include <string>
+
+#ifndef MAT4X4_H
+#define MAT4X4_h
+
+using namespace std;
+
+template <class T, class N> class Mat4x4 {
+    public:
+    T Mat4x4;
+// Default constructor
+    Mat4x4();
+// Constructar complex from its given real and imaginary parts
+    Mat4x4(const T coefficients[4][4]);
+
+// destructor
+    ~Mat4x4();
+
+// assignment operator
+    Mat4x4& operator= (const Mat4x4& other);
+
+// Refer to each element of the other
+    double& operator() (int i, int j);
+    double operator() (int i, int j) const;
+
+// addition
+    Mat4x4& operator+= (const Mat4x4& other);
+// subtraction
+    Mat4x4& operator-= (const Mat4x4& other);
+// multiplication
+    Mat4x4& operator*= (const Mat4x4& other);
+
+    const Mat4x4 operator- ();
+
+    friend ostream&
+        operator<<(ostream& os, const Mat4x4& m);
+
+    private:
+        N data[4][4];
+};
+
+// C=A+B
+    Mat4x4 operator+ (const Mat4x4& otherA, const Mat4x4& otherB); // 行列の加算
+    Mat4x4 operator- (const Mat4x4& otherA, const Mat4x4& otherB); // 行列の減算
+    Mat4x4 operator* (const Mat4x4& otherA, const Mat4x4& otherB); // 行列の乗算
+
+// if(A==B)
+    bool operator== (const Mat4x4& otherA, const Mat4x4& otherB);
+
+#endif
