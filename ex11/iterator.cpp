e53bec6746ef909c49203332174d4891852b4917
Hiroki-Nemoto <91859484+Hiroki-Nemoto@users.noreply.github.com>
2021-11-25 14:30:19 +0900

    Add files via upload

--- ex11/iterator.cpp
+++ ex11/iterator.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
 
+using namespace std;
+
+namespace PL4{
+    template <class FwdIt>
+    void sort(FwdIt begin, FwdIt end) {
+        for(auto i=begin; i!=end; ++i){
+            for(auto j=begin; j<i; ++j){
+                if(*i < *j){
+                    iter_swap(i, j);
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    vector<int> v{3, 7, 5, 1, 9};
+    PL4::sort(v.begin(),v.end());
+    for(int i=0; i<5; i++){
+        cout << v[i] << endl;
+    }
+    return 0;
+}
