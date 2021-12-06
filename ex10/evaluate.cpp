f266946219a91ac8cf80d89978280719419fb693
Hiroki-Nemoto <91859484+Hiroki-Nemoto@users.noreply.github.com>
2021-12-06 16:58:36 +0900

    Add files via upload

--- ex10/evaluate.cpp
+++ ex10/evaluate.cpp
@@ -7,66 +7,171 @@
 
 using namespace std;
 
-double evaluate(string data) {
-    stack<double> operand;
-    stack<string> operat;
+double evaluate(const string data){
+    double x, y, z;
+    char flag;
     stringstream ss(data);
+    stack<double> operand;
+    stack<char> operat;
     string d;
 
-    map<string, int> judge;
-    judge["+"] = 1;
-    judge["-"] = 1;
-    judge["*"] = 2;
-    judge["/"] = 2;
-    judge["("] = 0;
-    judge[")"] = 0;
+    map<char, int> judge;
+    judge['+'] = 1;
+    judge['-'] = 1;
+    judge['*'] = 2;
+    judge['/'] = 2;
+    judge['('] = 0;
+    judge[')'] = 0;
 
-    while(ss >> d) {
+    operat.push('\0');
+
+    while(ss >> d){
         if(d == "+" || d == "-" || d == "*" || d == "/"){
-            operat.push(d);
+            flag = *d.c_str();
+            operat.push(flag);
+            continue;
         }
-        else if(d == "(" ) {
+
+        else if(d == "("){
+            continue;
         }
-        else if(d == ")" ) {
-            double a1 = operand.top();
-            operand.pop();
-            double a2 = operand.top();
-            operand.pop();
-            string a3 = operat.top();
-            if(a3 == "+") {
+
+        else if(d == ")"){
+            if(operat.top() == '+'){
                 operat.pop();
-                double b1 = a2 + a1;
-                operand.push(b1);
-            } else if(a3 == "-") {
+                y = operand.top();
+                operand.pop();
+                x = operand.top();
+                operand.pop();
+                operand.push(x + y);
+            }
+
+            else if(operat.top() == '-'){
                 operat.pop();
-                double b2 = a2 - a1;
-                operand.push(b2);
-            } else if(a3 == "*") {
+                y = operand.top();
+                operand.pop();
+                x = operand.top();
+                operand.pop();
+                operand.push(x - y);
+            }
+
+            else if(operat.top() == '*'){
                 operat.pop();
-                double b3 = a2 * a1;
-                operand.push(b3);
-            } else if(a3 == "/") {
+
+                y = operand.top();
+                operand.pop();
+                x = operand.top();
+                operand.pop();
+                operand.push(x * y);
+            }
+
+            else if(operat.top() == '/'){
                 operat.pop();
-                double b4 = a1 / a2;
-                operand.push(b4);
+                y = operand.top();
+                operand.pop();
+                x = operand.top();
+                operand.pop();
+                operand.push(x / y);
+            }
+            continue;
+        }
+
+        else{
+            operand.push(stod(d));
+            continue;
+        }
+    }
+
+    while(operat.top() != '\0'){
+        if(judge[operat.top()] == 2){
+            if(operat.top() == '*'){
+                operat.pop();
+                y = operand.top();
+                operand.pop();
+                x = operand.top();
+                operand.pop();
+                operand.push(x * y);
+            }
+
+            else if(operat.top() == '/'){
+                operat.pop();
+                y = operand.top();
+                operand.pop();
+                x = operand.top();
+                operand.pop();
+                operand.push(x / y);
             }
         }
-        else operand.push(stod(d));
+
+        else if(judge[operat.top()] == 1){
+            flag = operat.top();
+            operat.pop();
+            if(judge[operat.top()] == 1 || operat.top() == '\0'){
+                if(flag == '+'){
+                    y = operand.top();
+                    operand.pop();
+                    x = operand.top();
+                    operand.pop();
+                    operand.push(x + y);
+                }
+
+                else if(flag == '-'){
+                    y = operand.top();
+                    operand.pop();
+                    x = operand.top();
+                    operand.pop();
+                    operand.push(x - y);
+                }
+            }
+
+            else if(judge[operat.top()] == 2){
+                z = operand.top();
+                operand.pop();
+                if(operat.top() == '*'){
+                    operat.pop();
+                    operat.push(flag);
+                    y = operand.top();
+                    operand.pop();
+                    x = operand.top();
+                    operand.pop();
+                    operand.push(x * y);
+                    operand.push(z);
+                    continue;
+                }
+
+                else if(operat.top() == '/'){
+                    operat.pop();
+                    operat.push(flag);
+                    y = operand.top();
+                    operand.pop();
+                    x = operand.top();
+                    operand.pop();
+                    operand.push(x / y);
+                    operand.push(z);
+                    continue;
+                }
+            }
+            continue;
+        }
     }
     return operand.top();
 }
 
-int main() {
+int main(){
     string data1 = "3 + 5 * 6 - 7 * ( 8 + 5 )";
-    string data2  = "25.0 * 12.0 / 5.0 - 8.0";
-    string data3 = "1 + 2 * 3 - 4 * ( 5 + 6 )";
+    string data2 = "25.0 * 12.0 / 5.0 - 8.0";
+    string data3 = "( 10.0 / 5.0 ) * 5 + 5 - ( 6.0 - 1.0 )";
 
-    cout << fixed << setprecision(1);
+    cout <<  fixed << setprecision(1);
 
     cout << "Input: " << data1 << endl;
-    cout << "output: " << evaluate(data1) << endl;
+    cout << "Output: "<< evaluate(data1) << endl;
+
     cout << "Input: " << data2 << endl;
-    cout << "output: " << evaluate(data2) << endl;
+    cout << "Output: "<< evaluate(data2) << endl;
+
     cout << "Input: " << data3 << endl;
-    cout << "output: " << evaluate(data3) << endl;
+    cout << "Output: "<< evaluate(data3) << endl;
+    return 0;
+
 }
